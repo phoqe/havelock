cc916c82a820dea11d5b141339b952a895a0af52
Linus Långberg <git@phoqe.dev>
2020-02-07 14:05:37 +0100

    Add docs

--- explorer.js
+++ explorer.js
@@ -2,6 +2,13 @@ const path = require("path");
 const fs = require("fs");
 const sqlite3 = require("sqlite3");
 
+/**
+ * Extracts data from an SQLite file located at `filePath` using the SQL statement `SELECT * FROM table`.
+ *
+ * @param filePath {string} The path to an SQLite file.
+ * @param table {string} The table to use in the `SELECT` statement, i.e. the table to extract data from.
+ * @returns {Promise<Array>} The rows selected from `table`.
+ */
 exports.getDataFromFile = (filePath, table) => {
   return new Promise((resolve, reject) => {
     if (!filePath) {
@@ -10,6 +17,7 @@ exports.getDataFromFile = (filePath, table) => {
       return;
     }
 
+    // Check if the file exists and is accessible.
     fs.access(filePath, fs.constants.R_OK, error => {
       if (error) {
         reject(error);
@@ -34,6 +42,7 @@ exports.getDataFromFile = (filePath, table) => {
               return;
             }
 
+            // Resolves the promise with all found entries in the table.
             resolve(rows);
           });
         }
@@ -42,6 +51,15 @@ exports.getDataFromFile = (filePath, table) => {
   });
 };
 
+/**
+ * Short-hand for `getDataFromFile()` that combines the parameters to a path.
+ *
+ * @param browser {object} A Havelock browser object, e.g. `browser.chromium`.
+ * @param profile {string} A profile where the user data of interest resides.
+ * @param file {string} The SQLite file to extract data from.
+ * @param table {string} The table to use in the `SELECT` statement, i.e. the table to extract data from.
+ * @returns {Promise<Array>} The rows selected from `table`.
+ */
 exports.getDataFromUserDataDirectoryFile = (browser, profile, file, table) => {
   if (!browser || !profile || !file || !table) {
     return;
@@ -50,4 +68,67 @@ exports.getDataFromUserDataDirectoryFile = (browser, profile, file, table) => {
   const filePath = path.join(browser.getUserDataDirectoryPath(), profile, file);
 
   return exports.getDataFromFile(filePath, table);
+};
+
+/**
+ * Short-hand for `getDataFromFile()` that combines the parameters to a path to the `Login Data` file using the table `logins`.
+ *
+ * @param browser {object} A Havelock browser object, e.g. `browser.chromium`.
+ * @param profile {string} A profile where the user data of interest resides.
+ * @returns {Promise<Array>} The rows selected from `logins`.
+ */
+exports.getLoginsFromLoginDataFile = (browser, profile) => {
+  if (!browser || !profile) {
+    return;
+  }
+
+  const filePath = path.join(
+    browser.getUserDataDirectoryPath(),
+    profile,
+    "Login Data"
+  );
+
+  return exports.getDataFromFile(filePath, "logins");
+};
+
+/**
+ * Short-hand for `getDataFromFile()` that combines the parameters to a path to the `Cookies` file using the table `cookies`.
+ *
+ * @param browser {object} A Havelock browser object, e.g. `browser.chromium`.
+ * @param profile {string} A profile where the user data of interest resides.
+ * @returns {Promise<Array>} The rows selected from `cookies`.
+ */
+exports.getCookiesFromCookiesFile = (browser, profile) => {
+  if (!browser || !profile) {
+    return;
+  }
+
+  const filePath = path.join(
+    browser.getUserDataDirectoryPath(),
+    profile,
+    "Cookies"
+  );
+
+  return exports.getDataFromFile(filePath, "cookies");
+};
+
+/**
+ * Short-hand for `getDataFromFile()` that combines the parameters to a path to the `History` file using the table `urls`.
+ *
+ * @param browser {object} A Havelock browser object, e.g. `browser.chromium`.
+ * @param profile {string} A profile where the user data of interest resides.
+ * @returns {Promise<Array>} The rows selected from `urls`.
+ */
+exports.getUrlsFromHistoryFile = (browser, profile) => {
+  if (!browser || !profile) {
+    return;
+  }
+
+  const filePath = path.join(
+    browser.getUserDataDirectoryPath(),
+    profile,
+    "History"
+  );
+
+  return exports.getDataFromFile(filePath, "urls");
 };
