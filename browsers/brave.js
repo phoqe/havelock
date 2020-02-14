5e54690f37693003aaf0b7a6af529ec08d215b8c
Linus Långberg <git@phoqe.dev>
2020-02-14 16:58:17 +0100

    Create brave.js

---
+++ browsers/brave.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+exports.id = "brave";
+exports.name = "Brave";
+exports.keychain = {
+  service: "Brave Safe Storage",
+  account: "Brave"
+};
+
+exports.getUserDataDirectoryPath = () => {
+  let userDataDirectoryPath;
+
+  if (process.platform === "win32") {
+    userDataDirectoryPath = path.join(
+      process.env.LOCALAPPDATA,
+      "BraveSoftware",
+      "Brave-Browser",
+      "User Data"
+    );
+  }
+
+  if (process.platform === "darwin") {
+    userDataDirectoryPath = path.join(
+      process.env.HOME,
+      "Library",
+      "Application Support",
+      "BraveSoftware",
+      "Brave-Browser"
+    );
+  }
+
+  if (process.platform === "linux") {
+    userDataDirectoryPath = path.join(process.env.HOME, ".config", "brave");
+  }
+
+  return userDataDirectoryPath;
+};
