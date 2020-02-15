a6bf9f3617bfa4fe8540a4ab983fb93e00518676
Linus Långberg <git@phoqe.dev>
2020-02-15 12:10:40 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+const havelock = require("./index");
+
+const explorer = havelock.explorer;
+const browser = havelock.browser;
+const security = havelock.security;
+
+explorer
+  .getLoginsFromLoginDataFile(browser.chrome, "Profile 1")
+  .then(logins => {
+    logins.forEach(login => {
+      security
+        .decryptData(browser.chrome, login.password_value)
+        .then(value => {
+          console.log(value);
+        })
+        .catch(reason => {
+          console.error(reason);
+        });
+    });
+  })
+  .catch(reason => {
+    console.error(reason);
+  });
