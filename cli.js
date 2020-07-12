07c4abaee8b5882778e97e10b67ca2832cb2afa1
Linus Långberg <git@phoqe.dev>
2020-07-12 17:19:01 +0200

    Add option to table data

--- cli.js
+++ cli.js
@@ -7,36 +7,72 @@ const package = require("./package.json");
 program.name(package.name);
 program.version(package.version);
 
-program.option(
-  "-d, --decrypt",
-  "decrypt fields known to be encrypted, i.e. `password_value`"
-);
+program
+  .option(
+    "-d, --decrypt",
+    "decrypt fields known to be encrypted, i.e. `password_value`"
+  )
+  .option("-t, --table", "output interesting data in a table");
 
 program
   .command("logins <browser> <profile>")
   .alias("accounts")
   .action((browser, profile) => {
-    havelock.explorer.printLoginsFromLoginDataFile(
-      havelock.browser[browser],
-      profile
-    );
+    havelock.explorer
+      .getLoginsFromLoginDataFile(havelock.browser[browser], profile)
+      .then((value) => {
+        if (program.table) {
+          console.table(value, [
+            "origin_url",
+            "username_value",
+            "password_value",
+          ]);
+
+          return;
+        }
+
+        console.log(value);
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
   });
 
 program.command("cookies <browser> <profile>").action((browser, profile) => {
-  havelock.explorer.printCookiesFromCookiesFile(
-    havelock.browser[browser],
-    profile
-  );
+  havelock.explorer
+    .getCookiesFromCookiesFile(havelock.browser[browser], profile)
+    .then((value) => {
+      if (program.table) {
+        console.table(value, ["host_key", "name", "encrypted_value"]);
+
+        return;
+      }
+
+      console.log(value);
+    })
+    .catch((reason) => {
+      console.error(reason);
+    });
 });
 
 program
   .command("urls <browser> <profile>")
   .alias("history")
   .action((browser, profile) => {
-    havelock.explorer.printUrlsFromHistoryFile(
-      havelock.browser[browser],
-      profile
-    );
+    havelock.explorer
+      .getUrlsFromHistoryFile(havelock.browser[browser], profile)
+      .then((value) => {
+        if (program.table) {
+          console.table(value, ["url", "title"]);
+
+          return;
+        }
+
+        console.log(value);
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
   });
 
 program.parse(process.argv);
