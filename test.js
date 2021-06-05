fe6c6f515a4234f3715fa36e023df346b6863296
Linus Långberg <git@phoqe.dev>
2021-06-05 21:34:38 +0200

    feat: add tests

---
+++ test.js
@@ -0,0 +1,28 @@
+const havelock = require("./havelock");
+
+const explorer = havelock.explorer;
+const browser = havelock.browser;
+const crypto = havelock.crypto;
+
+explorer
+  .getDataFromUserDataDirectoryFile(
+    browser.chrome,
+    "Default",
+    "Login Data",
+    "logins"
+  )
+  .then((logins) => {
+    logins.forEach((login) => {
+      crypto
+        .decrypt(browser.chrome, login.password_value)
+        .then((value) => {
+          console.log(value);
+        })
+        .catch((reason) => {
+          console.error(reason);
+        });
+    });
+  })
+  .catch((reason) => {
+    console.error(reason);
+  });
